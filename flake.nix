8e05e86a23696a295086ebdcfa8bd661b9147c63
Emre Bicer <oebicer@gmail.com>
2024-10-23 13:32:01 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "BtrBlocks git development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
+
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+              pandas
+              numpy
+            ]))
+              python311Packages.cmake
+              gnumake
+              zlib
+              openssl_legacy
+              curlFull
+              ccls
+              gflags
+              yaml-cpp
+              spdlog
+              tbb
+              nix-ld
+          ];
+        };
+
+    };
+
+}
